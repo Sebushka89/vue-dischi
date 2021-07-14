47bf765ef7f99d46ed753b56141c5edf6b9b3f30
Sebushka89 <sebastian.rizzo@hotmail.it>
2021-07-14 14:19:28 +0200

    terzo commit

---
+++ src/components/Discs.vue
@@ -0,0 +1,21 @@
+<template>
+   <div></div>
+</template>
+
+<script>
+export default {
+    name: 'Discs' ,
+    props:{
+       poster: String,
+       title: String,
+       author: String,
+       genre: String,
+       year: String
+    }
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <Discs v-for="(disc, index) in discs" :key=index
+        :poster='discs.poster'
+        :title='discs.title'
+        :author='discs.author'
+        :genre='discs.genre'
+        :year='discs.year' />
+    </div>
+</template>
+
+<script>
+
+import Discs from './Discs.vue'
+
+export default {
+    name: 'Main',
+    components: {
+        Discs
+    },
+    props: {
+        discs: Array
+    }
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
